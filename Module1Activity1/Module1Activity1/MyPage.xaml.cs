e39b687acf8d69a2fa03bba9b6eca5f6e75d4bf1
kncstlln <kane@Kanes-MacBook-Air.local>
2023-09-06 22:25:56 +0800

    first commit

---
+++ Module1Activity1/Module1Activity1/MyPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module1Activity1
+{	
+	public partial class MyPage : ContentPage
+	{	
+		public MyPage ()
+		{
+			InitializeComponent ();
+
+
+		}
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
+
